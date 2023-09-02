ca7edc1d5087944404579835f0ebb50993dd805c
BelloDev <fernandojbf123@gmail.com>
2023-09-01 23:21:50 -0700

    update: adding more features to the project

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
-
+import Head from "next/head"
+import Image from "next/image"
+import {PrismaClient} from "@prisma/client"
 
 export default function Home() {
   return (
      <h1>DESDE INDEX JS</h1>
   )
+}
+
+export const getServerSideProps = async () => {
+  const prisma = new PrismaClient()
+
+  const categories = await prisma.category.findMany();
+  console.log(categories)
 }
