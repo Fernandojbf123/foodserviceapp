d56d886b21d653d08e361d691de0e69aee1fffeb
BelloDev <fernandojbf123@gmail.com>
2023-09-06 14:34:42 -0700

    fixed: small bugs when POST request to API

--- pages/api/kitchenorders.js
+++ pages/api/kitchenorders.js
@@ -4,18 +4,18 @@ export default async function handler(req,res) {
     const prisma = new PrismaClient()
 
     //Fetch orders
-    const kitchenorders = await prisma.kitchen.findMany( {
-        where:{
-            status: false
-        }
-    })
-    res.status(200).json(kitchenorders)
+    if (req.method === "GET") {
+        const kitchenorders = await prisma.kitchen.findMany( {
+            where:{
+                status: false
+            }
+        })
+        res.status(200).json(kitchenorders)
+    }
 
 
     //Create an order
     if (req.method === "POST") {
-        console.log("METODO POST")
-        console.log(req.body)
         const kitchenorders = await prisma.kitchen.create ({
             data: {
                 clientName: req.body.clientName,
